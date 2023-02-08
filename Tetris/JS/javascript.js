d26229c9d921109221103f0e595a883ad07b9a5d
Jiho <qkrwlgh21c@naver.com>
2023-02-08 19:11:42 +0900

    Tetris2

---
+++ Tetris/JS/javascript.js
@@ -0,0 +1,22 @@
+// 함수 형태로 저장하여 html에서 적용
+
+// 즉시 실행 함수 사용하여 이름 받기
+(function() {
+  var userName = prompt("이름을 입력하세요!");
+  
+  if (userName === null) {
+    alert("유효하지 않은 이름입니다!");
+    location.reload(true);
+  }
+
+  if ((userName.length < 6) && (userName.length > 0)) {
+    document.getElementById("inputName").innerHTML = "name : " + userName; 
+
+  } else {
+    alert("유효하지 않은 이름입니다!");
+    location.reload(true);
+  }  
+}())
+
+
+
